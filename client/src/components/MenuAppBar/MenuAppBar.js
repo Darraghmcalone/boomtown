40d041aba56fa4274f0e19f31ae68e5328cdba62
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-10-21 10:33:07 +0100

    add menu app bar drop down; insert navigation links to profile and logout

--- client/src/components/MenuAppBar/MenuAppBar.js
+++ client/src/components/MenuAppBar/MenuAppBar.js
@@ -1,8 +1,68 @@
 import React from 'react'
+import IconButton from '@material-ui/core/IconButton'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import AuthContainer from '../../containers/AuthContainer'
+import { Link } from 'react-router-dom'
+import { ViewerContext } from '../../context/ViewerProvider'
+
+const ITEM_HEIGHT = 48
 
 class MenuAppBar extends React.Component {
+  state = {
+    anchorEl: null
+  }
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget })
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: null })
+  }
+
   render() {
-    return <div>Menu App Bar</div>
+    const { anchorEl } = this.state
+
+    return (
+      <div>
+        <IconButton
+          aria-label="More"
+          aria-owns={anchorEl ? 'long-menu' : null}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: 200
+            }
+          }}
+        >
+          <ViewerContext.Consumer>
+            {({ viewer }) => (
+              <Link to={`/profile`}>
+                <MenuItem onClick={this.handleClose}> Your Profile </MenuItem>
+              </Link>
+            )}
+          </ViewerContext.Consumer>
+          <AuthContainer>
+            {({ logout }) => {
+              return <MenuItem> Log Out</MenuItem>
+            }}
+          </AuthContainer>
+          ))}
+        </Menu>
+      </div>
+    )
   }
 }
 
